918701f6bb2a7e20836e0bf7d8c4cb16c6783f82
alanmac17 <alan17wilson@gmail.com>
2022-05-01 21:18:29 +0000

    paper rock scissors tutorial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,52 @@
-const computerChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('user-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+
+let userChoice;
+let computerChoice;
+let result;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener ('click', (e) => {
+   userChoice = e.target.id
+   userChoiceDisplay.innerHTML = userChoice
+   generateComputerChoice()
+   getResult()
+} ))
+
+function generateComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'It is a draw' 
+    } else if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'You win!'
+    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'You lost!'
+    } else if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'You win!'
+    } else if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'You lost!'
+    } else if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'You lost!'
+    } else if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'You win!' } else { 
+            (result = 'invalid')
+        }
+    
+    resultDisplay.innerHTML = result;
+}
